77fe9fbe9820297ac4d1d1269cd352ca525cb935
jsebguerrero <jsebastian.guerrero@udea.edu.co>
2019-08-02 18:56:52 -0500

    added animations

--- app/src/main/java/com/nestorcalvo/logininterface/SplashActivity.kt
+++ app/src/main/java/com/nestorcalvo/logininterface/SplashActivity.kt
@@ -4,23 +4,34 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 
 class SplashActivity : Activity() {
-
     private lateinit var handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        handler = Handler()
+
+        //var bgapp = findViewById<ImageView>(R.id.bgapp)
+
+        //var bganim = AnimationUtils.loadAnimation(this,R.anim.bganim)
+
+        //bgapp.startAnimation(bganim)
+
+        //bgapp.animate().translationX((-15000).toFloat()).setDuration(1000).setStartDelay(1500)
+
+
+       /* handler = Handler()
         handler.postDelayed({
             val intent = Intent(this@SplashActivity, LoginActivity::class.java)
             intent.putExtra("flag",true)
             startActivity(intent)
             finish()
-        }, 1500)
+        }, 1500)*/
 
     }
 }
